a036a4ef959b9800283e52f0da78162b245288b3
floydiology <john.newbury@gmail.com>
2021-07-25 11:22:41 -0700

    test deploy

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function navbar() {
+  return (
+    <div className="header">
+      <h1> John Newbury</h1>
+      <h2> Creative Front End Web Developer</h2>
+      <div className="introduction">
+        <p>
+          Experienced front end web developer and freelancer interested in
+          migrating to full-time employment with a supportive and engaging
+          organization. Strong reputation for innovation, high standards of
+          excellence, creative solutions, and continuous learning.
+        </p>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,58 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/Image";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>John Newbury - Creative Front End Developer</title>
       </Head>
-      <main></main>
-      <footer></footer>
-    </div>
+      <main className="container">
+        <span className="section__header">
+          <h2>Toolbox</h2>
+        </span>
+        <section class="card-list">
+          <article class="card">
+            <header class="card-header">
+              <h2>Developer Tools</h2>
+            </header>
+            <div>
+              <ul>
+                <li>React - Next</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>javaScript</li>
+                <li>VS Code</li>
+              </ul>
+            </div>
+            <div class="tags">
+              <a href="#">html</a>
+              <a href="#">css</a>
+              <a href="#">web-dev</a>
+            </div>
+          </article>
+          <article class="card">
+            <header class="card-header">
+              <h2>Designer Tools</h2>
+            </header>
+            <div>
+              <h3>Adobe CC</h3>
+              <ul>
+                <li>Photoshop</li>
+                <li>Illustrator</li>
+                <li>In-Design</li>
+                <li>Affinity Designer</li>
+              </ul>
+            </div>
+            <div class="tags">
+              <a href="#">html</a>
+              <a href="#">css</a>
+              <a href="#">design</a>
+            </div>
+          </article>
+        </section>
+      </main>
+    </>
   );
 }
