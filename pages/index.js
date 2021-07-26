db280efc965b11fcef816fb436b68443a66803fa
floydiology <john.newbury@gmail.com>
2021-07-26 10:38:59 -0700

    new

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,12 @@ export default function Home() {
         <span className="section__header">
           <h2>Toolbox</h2>
         </span>
-        <section class="card-list">
-          <article class="card">
-            <header class="card-header">
+        <section className="card-list">
+          <article className="card">
+            <header className="card-header">
               <h2>Developer Tools</h2>
             </header>
-            <div>
+            <div className="card-body">
               <ul>
                 <li>React - Next</li>
                 <li>HTML</li>
@@ -32,20 +32,25 @@ export default function Home() {
               <a href="#">web-dev</a>
             </div>
           </article>
-          <article class="card">
-            <header class="card-header">
+          <article className="card">
+            <header className="card-header">
               <h2>Designer Tools</h2>
             </header>
-            <div>
+            <div className="card-body">
               <h3>Adobe CC</h3>
+              <p>
+                Expert level with over 25 years experience with the following
+                list of tools:
+              </p>
               <ul>
                 <li>Photoshop</li>
                 <li>Illustrator</li>
                 <li>In-Design</li>
-                <li>Affinity Designer</li>
+                <li>After Effect</li>
+                <li>Premiere</li>
               </ul>
             </div>
-            <div class="tags">
+            <div className="tags">
               <a href="#">html</a>
               <a href="#">css</a>
               <a href="#">design</a>
